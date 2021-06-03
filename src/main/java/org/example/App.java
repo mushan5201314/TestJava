42455bba4028f343718bc43a7f287b481c54766f
“mushan5201314” <262655878@qq.com>
2021-06-03 13:49:34 +0800

    123

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Wor3ewqld!" );
+        System.out.println( "Hello Worewq3ewqld!" );
     }
 }
